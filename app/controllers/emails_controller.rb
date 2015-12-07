0504f7bc66787a4cd0779fbeeb10fdc8717753ec
Rana Waleed <waleedsulehria@gmail.com>
2015-12-08 04:10:04 +0500

    will send a new email and add entreis to database

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -12,16 +12,16 @@ class EmailsController < ApplicationController
     email.body = params['body-plain'];
     email.save;
 
-    #mg_client = Mailgun::Client.new "key-5b10854538566549aac6724aaa54dabe"
+    mg_client = Mailgun::Client.new "key-5b10854538566549aac6724aaa54dabe"
 
     #Define your message parameters
-    #message_params = {:from    => 'waleedsulehria@gmail.com',
-    #                  :to      => 'aminshahgilani@gmail.com',
-    #                  :subject => 'The Ruby SDK is awesome!',
-    #                  :text    => 'It is really easy to send a message!'}
+    message_params = {:from    => 'waleed@gmail.com',
+                      :to      => email.from,
+                      :subject => 'Coinbase address',
+                      :text    => 'Pay the amount of reward in BTC here:XXXXXXXXXXX'}
 
     #Send your message through the client
-    #mg_client.send_message "sandbox050314df0b744b97beecf2742a588852.mailgun.org", message_params
+    mg_client.send_message "sandbox050314df0b744b97beecf2742a588852.mailgun.org", message_params
 
     render template: "emails/recieve"
 
