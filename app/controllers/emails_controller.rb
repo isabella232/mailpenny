8f383edc7be54f2ddaa0bec68c0bfae02fb33233
RANA WALEED ASMAT <waleedsulehria@gmail.com>
2015-12-19 20:48:31 +0500

    kuch kiya tou thhaa

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,18 +2,13 @@
 class EmailsController < ApplicationController
   def recieve
 
-      email = Email.find_by_to(params['from'].to_s)
-
+    email = Email.find_by_to(params['from'].to_s)
       if(email.present?)
-        if(params['subject'].to_s.include? 'Re: ')
-          str = params['subject'].to_s.slice 'Re: '
-          if(str == email.subject)
-            transaction = Transaction.find_by_email_id(email.id)
-            to = email.to
-            amount = transaction.amount.to_s
-            send_money('b2411493-3d92-5c11-b6ad-aee0a0a446a7',amount,to)
-          end
-        end
+
+        transaction = Transaction.find_by_email_id(email.id)
+        to = email.to
+        amount = transaction.amount.to_s
+        send_money('b2411493-3d92-5c11-b6ad-aee0a0a446a7',amount,to)
       else
         user = User.find_by_email(params['from'])
         if(user.present?)
