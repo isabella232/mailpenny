b6f73de254ee5dfb5a4690af91f5cfc143ec43ad
Amin Shah Gilani <aminshahgilani@gmail.com>
2016-03-14 06:08:57 -0700

    Billing was routed twice

--- app/helpers/paywall_helper.rb
+++ app/helpers/paywall_helper.rb
@@ -1,2 +1,10 @@
 module PaywallHelper
+  # Returns the Gravatar (http://gravatar.com/) for the given user.
+  def gravatar_for(user, options = { size: 150 })
+    md5 = Digest::MD5
+    gravatar_id = md5.hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    gravatar_url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
   #get 'emails/welcome'        => 'emails#welcome'
   #get 'emails/create_address' => 'emails#create_address'
   #post 'emails/payment_recieved' => 'emails#payment_recieved'
-  get 'billing' => 'paywall#billing'
 
   post 'recieve' => 'paywall#recieve'
   get 'register' => 'paywall#register'
