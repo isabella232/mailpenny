bcc314c7492f0f1274a80703b2d619bf69540c0d
Amin Shah Gilani <aminshahgilani@gmail.com>
2016-03-16 09:57:57 -0700

    Fixed the syntax for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@
 # (or created alongside the db with db:setup).
 
 # The meta accounts to track money and stuff
-Account.create([{ meta: true }, { meta_name: 'withdrawal' }])
-Account.create([{ meta: true }, { meta_name: 'deposit' }])
-Account.create([{ meta: true }, { meta_name: 'revenue' }])
+Account.create(meta: true, meta_name: 'withdrawal')
+Account.create(meta: true, meta_name: 'deposit')
+Account.create(meta: true, meta_name: 'revenue')
