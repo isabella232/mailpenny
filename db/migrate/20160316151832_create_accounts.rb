9b2abc71f0b84393b538df6fbfdb1a59975317d8
Amin Shah Gilani <aminshahgilani@gmail.com>
2016-03-16 08:26:10 -0700

    Add balance to the accounts

--- db/migrate/20160316151832_create_accounts.rb
+++ db/migrate/20160316151832_create_accounts.rb
@@ -1,7 +1,8 @@
 class CreateAccounts < ActiveRecord::Migration
   def change
     create_table :accounts do |t|
-
+      t.integer 'user_id'
+      t.decimal 'balance'
       t.timestamps null: false
     end
   end
