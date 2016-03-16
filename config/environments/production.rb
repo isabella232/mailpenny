f71263dcca82c1a658329cb5832b4d85a5c6b412
Amin Shah Gilani <aminshahgilani@gmail.com>
2016-03-16 06:51:18 -0700

    Finish installing devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Because in production, we're themailman.io, duhhhh
+  config.action_mailer.default_url_options = { host: 'themailman.io' }
 end
