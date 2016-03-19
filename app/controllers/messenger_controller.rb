5538bba2c209ed1679b89f4b4ab43a4bd84bc5ef
waleed <waleedsulehria@gmail.com>
2016-03-19 16:29:30 -0700

    error fix

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -1,11 +1,6 @@
 class MessengerController < ApplicationController
-	
-
 	def mail
-		if(params.include?'subject' && params.incluse?'message')
-			user = current_human
-			email = user.email;
-		end
+		
 	end
 	def sms
 
