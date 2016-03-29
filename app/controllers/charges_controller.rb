c79df81cbc4ee2f1399b82b37dd343ae9597bbb3
waleed <waleedsulehria@gmail.com>
2016-03-28 22:20:51 -0700

    bug fixes with stripe

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,6 +1,6 @@
 # The stripe charges controller
 class ChargesController < ApplicationController
-  before_action :set_user_if_in_session
+  before_action :authenticate_human!
 
   def new
     amount = params[:amount].to_i
@@ -10,7 +10,9 @@ class ChargesController < ApplicationController
 
   def create
     # Amount in cents
-    @amount = params[:amount].to_i
+    @amount = params[:amount].to_f
+    @amount = @amount.to_f
+    @amount = @amount/100.to_f
     @email = params[:stripeEmail]
     @token = params[:stripeToken]
     customer = Stripe::Customer.create(
@@ -20,52 +22,15 @@ class ChargesController < ApplicationController
 
     charge = Stripe::Charge.create(
       customer: customer.id,
-      amount: @amount,
+      amount: (@amount.to_i * 100),
       description: "#{@amount} topup for #{@email}",
       currency: 'usd'
     )
 
-    deposit to: @current_user, amount: @amount, stripe_charge_id: charge.id
+    current_human.account.deposit amount: @amount, stripe_charge_id: charge.id
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
-  end
-
-  private
-
-  def deposit(args)
-    args.slice!(:to,
-                :amount,
-                :currency,
-                :ref,
-                :meta,
-                :stripe_charge_id
-               )
-    args[:deposit] = true
-    add_ledger_entry args
-  end
-
-  def withdraw(args)
-    args.slice!(:from,
-                :amount,
-                :currency,
-                :ref,
-                :meta
-               )
-    args[:withdrawal] = true
-    add_ledger_entry args
-  end
-
-  def make_payment(args)
-    args.slice!(:from,
-                :to,
-                :amount,
-                :currency,
-                :ref,
-                :meta
-               )
-    args[:payment] = true
-    add_ledger_entry args
   end
 end
