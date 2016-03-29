f5e12c9d98cc7cd69ce474fe8b5ebfcfb2de3682
waleed <waleedsulehria@gmail.com>
2016-03-29 01:38:35 -0700

    model fix

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,5 +1,5 @@
 class Reward < ActiveRecord::Base
   belongs_to :human
   validates :sms ,:numericality => { :greater_than => 0}
-  validates :email ,:numericality => { :greater_than => 0}
+  validates :email ,:numericality => { :greater_than_or_equal_to => 0}
 end
