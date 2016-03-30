80c47ab06a3406d58ca90297df5546c0dea262cf
waleed <waleedsulehria@gmail.com>
2016-03-29 21:06:58 -0700

    fix

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,5 +1,12 @@
 class Reward < ActiveRecord::Base
   belongs_to :human
+  after_initialize :init
+
+  def init
+    self.sms = 1
+    self.email = 1
+  end
+
   validates :sms ,:numericality => { :greater_than => 0}
   validates :email ,:numericality => { :greater_than_or_equal_to => 0}
 end
