8e13fb81abe0eefd8ae99008211c729267ab8340
Amin Shah Gilani <aminshahgilani@gmail.com>
2016-04-19 03:41:44 -0700

    remove redundent relationship from human model

--- app/models/human.rb
+++ app/models/human.rb
@@ -2,7 +2,7 @@
 class Human < ActiveRecord::Base
   require 'rails_restricted_usernames'
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :lockable,
          :confirmable
@@ -14,15 +14,9 @@ class Human < ActiveRecord::Base
   has_one :reward
   has_one :social_medium
   has_many :ccards
-  has_many :emails
-  has_many :phones
-  has_many :whitelists
-  has_many :user_emails
   before_create :build_default_profile
   before_create :build_default_account
   before_create :build_default_profile
-  before_create :build_default_reward
-  before_create :build_default_social_medium
 
   # The money movement
   def fee
@@ -32,16 +26,6 @@ class Human < ActiveRecord::Base
   end
 
   private
-
-  def build_default_reward
-    build_reward
-    true
-  end
-
-  def build_default_social_medium
-    build_social_medium
-    true
-  end
 
   def build_default_account
     build_account
