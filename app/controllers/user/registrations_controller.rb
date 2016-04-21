ee8230278fb63675d1560dbdbb9d3b9130eab988
Amin Shah Gilani <aminshahgilani@gmail.com>
2016-04-21 08:07:26 -0700

    Update username

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,12 +1,12 @@
 class User::RegistrationsController < Devise::RegistrationsController
   layout 'dashboard', only: [:edit]
   before_filter :configure_sign_up_params, only: [:create]
-  # before_filter :configure_account_update_params, only: [:update]
+  before_filter :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource
    def create
@@ -55,7 +55,7 @@ class User::RegistrationsController < Devise::RegistrationsController
    end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    super(resource)
+  end
 end
