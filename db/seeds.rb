53270966894534b383b4ccfa72b57cf35253a0a0
Amin Shah Gilani <aminshahgilani@gmail.com>
2016-04-26 07:31:03 -0700

    Add dummy user for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 Account.create(meta: true, meta_name: 'withdrawal')
 Account.create(meta: true, meta_name: 'deposit')
 Account.create(meta: true, meta_name: 'revenue')
+
+# To make developer lives easier, create a dummy user for development:
+unless Rails.env == 'production'
+  User.create(
+    username: 'dummy',
+    email: 'email@example.com',
+    password: 'password',
+    password_confirmation: 'password',
+    confirmed_at: Date.today
+  )
+end
