2836c0093e57ad72e676d54fa4200c1a5de89a91
Amin Shah Gilani <aminshahgilani@gmail.com>
2016-04-28 20:14:20 -0700

    Add valid formating to the username and fix stripe id generator

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   validates :username,
             presence: true,
             uniqueness: { case_sensitive: false },
-            format: { with: /\A[a-zA-Z0-9]+\Z/ },
+            format: { with: /\A[a-zA-Z0-9_.-]+\Z/ },
             exclusion: {
               in: RestricedUsernamesIdentifier.new.initial_path_segments,
               message: 'That username is unavailable'
@@ -24,13 +24,25 @@ class User < ActiveRecord::Base
   has_many :ccards
   before_create :build_default_profile
   before_create :build_default_account
-  before_create :generate_stripe_id
 
   # The money movement
   def fee
     fee = BigDecimal('0.5')
     fee = BigDecimal('0.2') if charitable
     fee
+  end
+
+  def generate_stripe_id
+    if stripe_customer_id.blank?
+      stripe = Stripe::Customer.create(
+        description: "Username: #{username}",
+        email: email
+      )
+      self.stripe_customer_id = stripe.id
+      save!
+    else
+      fail 'This user already has a Stripe ID'
+    end
   end
 
   private
@@ -43,13 +55,5 @@ class User < ActiveRecord::Base
   def build_default_profile
     build_profile
     true
-  end
-
-  def generate_stripe_id
-    stripe = Stripe::Customer.create(
-      description: "Username: #{username}",
-      email: email
-    )
-    self.stripe_customer_id = stripe.id
   end
 end
