2177f832191c8710b59518d219af77eb499ef1c0
amingilani <aminshahgilani@gmail.com>
2016-07-11 06:22:42 +0500

    Add custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  as :user do
+    get 'login' => 'users/sessions#new', as: :login_page
+    post 'login' => 'users/sessions#create'
+    delete 'logout' => 'users/sessions#destroy'
+    get 'register' => 'users/registrations#new', as: :register_page
+  end
 end
