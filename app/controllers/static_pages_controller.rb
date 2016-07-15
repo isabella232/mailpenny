872864104ab666d2752a66f8bdaef24d04f0fd27
amingilani <aminshahgilani@gmail.com>
2016-07-15 21:17:07 +0500

    Generate the static pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,17 @@
+# Pages visible when not signed in
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/terms'
+
+  get 'static_pages/privacy'
+
+  get 'static_pages/help'
+
   devise_for :users
 
   as :user do
