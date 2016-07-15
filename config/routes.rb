bf9e4c02644fb911cc2a9a40a1eca09dcf6f4fd4
amingilani <aminshahgilani@gmail.com>
2016-07-15 21:43:53 +0500

    Fix routes for static pagez

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,18 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  # static pages are public pages
+  root to: redirect('/home') # redirect root to /home
+  get 'home'    => 'static_pages#home'
+  get 'about'   => 'static_pages#about'
+  get 'terms'   => 'static_pages#terms'
+  get 'privacy' => 'static_pages#privacy'
+  get 'help'    => 'static_pages#help'
 
-  get 'static_pages/about'
-
-  get 'static_pages/terms'
-
-  get 'static_pages/privacy'
-
-  get 'static_pages/help'
-
+  # user signups and registration
   devise_for :users
-
   as :user do
-    get 'login' => 'users/sessions#new', as: :login_page
-    post 'login' => 'users/sessions#create'
-    delete 'logout' => 'users/sessions#destroy'
-    get 'register' => 'users/registrations#new', as: :register_page
+    get    'login'    => 'users/sessions#new', as: :login_page
+    post   'login'    => 'users/sessions#create'
+    delete 'logout'   => 'users/sessions#destroy'
+    get    'register' => 'users/registrations#new', as: :register_page
   end
 end
