7fdb70c75a41ec3a95c846baeda9198265ed2191
amingilani <aminshahgilani@gmail.com>
2016-08-01 10:29:33 +0500

    Fix redirects after login and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # The parent controller for all controllers
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # redirect users after login
+  def after_sign_in_path_for(_resource)
+    dashboard_overview_path
+  end
+
+  # redirect users after logout
+  def after_sign_out_path_for(_resource)
+    home_page_path
+  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -20,14 +20,6 @@ class Users::SessionsController < Devise::SessionsController
 
   protected
 
-  def after_sign_in_path_for(_resource)
-    root_path
-  end
-
-  def after_sign_out_path_for(_resource)
-    root_path
-  end
-
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
