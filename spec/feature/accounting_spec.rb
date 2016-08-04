629a7cd3af5e1ef7dc8a33dd4c65d26a3788f31d
Amin Shah Gilani <aminshahgilani@gmail.com>
2016-08-04 05:22:43 -0700

    Add account test for deposits

--- spec/feature/accounting_spec.rb
+++ spec/feature/accounting_spec.rb
@@ -9,4 +9,13 @@ feature 'Accounting' do
   scenario 'user must have a default account' do
     expect(@alice.account).not_to be_nil
   end
+
+  scenario 'user balance must not be nil' do
+    expect(@alice.account.balance).not_to be_nil
+  end
+
+  scenario 'deposits must increase balance' do
+    @alice.account.deposit 500
+    expect(@alice.account.balance).to eq(500)
+  end
 end
