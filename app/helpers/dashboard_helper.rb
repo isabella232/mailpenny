1f9ed5e6ea5f0de4f5a55b40865a75b085d6f0cb
Amin Shah Gilani <aminshahgilani@gmail.com>
2016-08-03 20:56:08 -0700

    Add and pass tests for username change in account settings

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,3 +1,14 @@
 # helper module for the dashboard controller
 module DashboardHelper
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= current_user
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
---
+++ spec/feature/dashboard_account_settings_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+feature 'Dashboard' do
+  background do
+    @user = create(:user) # create a new user
+    @user_changed = build(:user) # build an alternative user
+
+    # login as @user
+    visit '/login'
+    within('#new_user') do
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+    end
+    click_button 'Log in'
+  end
+
+  context 'in account settings' do
+    scenario 'change username successfully' do
+      visit '/dashboard/account'
+      within '#edit_user' do
+        fill_in 'Username', with: @user_changed.username
+        fill_in 'Current password', with: @user.password
+      end
+      click_button 'Update'
+      expect(page).to have_content 'Your account has been updated successfully.'
+    end
+
+    scenario 'fail to change username on incorrect password' do
+      visit '/dashboard/account'
+      within '#edit_user' do
+        fill_in 'Username', with: @user_changed.username
+        fill_in 'Current password', with: "incorrect_#{@user.password}"
+      end
+      click_button 'Update'
+      expect(page).to have_content 'password is invalid'
+    end
+  end
+end
