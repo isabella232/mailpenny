784aae14c69b9a3d70c2e89c48855f7d98241df2
Amin Shah Gilani <aminshahgilani@gmail.com>
2016-08-04 00:35:47 -0700

    Migrate accounts table

--- db/migrate/20160804073212_create_accounts.rb
+++ db/migrate/20160804073212_create_accounts.rb
@@ -1,8 +1,8 @@
 class CreateAccounts < ActiveRecord::Migration[5.0]
   def change
     create_table :accounts do |t|
-      t.decimal :balance
-      t.integer :type
+      t.decimal :balance, default: 0
+      t.integer :type, null: false
       t.references :user, foreign_key: true
 
       t.timestamps
