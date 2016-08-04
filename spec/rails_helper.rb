7508de9c9888b2019631f0bf38caf20cef4631eb
Amin Shah Gilani <aminshahgilani@gmail.com>
2016-08-04 05:21:47 -0700

    Seed test db before running rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,6 +28,11 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+
+  config.before(:suite) do
+    Rails.application.load_seed # loading seeds
+  end
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
