a6f72c42d3005e930bd56e46833e69953dba601f
Amin Shah Gilani <aminshahgilani@gmail.com>
2016-08-04 23:22:11 -0700

    Rmove extra whitespace

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,7 +28,6 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-
   config.before(:suite) do
     Rails.application.load_seed # loading seeds
   end
