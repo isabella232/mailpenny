3035a8207c7ce4e43182211471d144be09b6f82d
Amin Shah Gilani <aminshahgilani@gmail.com>
2016-08-05 03:34:05 -0700

    Add validation tests

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -14,5 +14,21 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when validating' do
+    it 'should be invalid without a transaction_type' do
+      tx = Transaction.new
+      tx.valid?
+      expect(tx.invalid?).to be true
+    end
+    it 'should be invalid without from_id and to_id accounts' do
+      tx = Transaction.new
+      tx.valid?
+      expect(
+        tx.errors.keys.include?(:account) &&
+        tx.errors.keys.include?(:from_id) &&
+        tx.errors.keys.include?(:to_id) &&
+        tx.errors.messages[:account].count == 2
+      ).to be true
+    end
+  end
 end
