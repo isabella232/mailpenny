e9fb5cd90567b5332c1bd273fc5be3fcb9c13def
amingilani <aminshahgilani@gmail.com>
2016-08-22 23:31:50 +0500

    Annotate phone_number model

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: phone_numbers
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer          not null
+#  country_code :string           not null
+#  phone_number :string           not null
+#  authy_id     :string           not null
+#  confirmed    :boolean          default(FALSE), not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe PhoneNumber, type: :model do
