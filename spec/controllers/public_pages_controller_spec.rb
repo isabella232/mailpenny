2808eddf12161f5bd78f87b3e7883ba6e655529e
amingilani <aminshahgilani@gmail.com>
2016-08-22 16:13:50 +0500

    Fix test for user profile

--- spec/controllers/public_pages_controller_spec.rb
+++ spec/controllers/public_pages_controller_spec.rb
@@ -8,9 +8,10 @@ RSpec.describe PublicPagesController, type: :controller do
     end
   end
 
-  describe 'GET #profile' do
+  describe 'GET #profile for user with profile' do
     it 'returns http success' do
       user = create(:user)
+      user.profile = build :profile
       get :profile, params: { username: user.username }
       expect(response).to have_http_status(:success)
     end
