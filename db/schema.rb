76a72861942fae1caabb7476125ac6a2cd723e93
amingilani <aminshahgilani@gmail.com>
2016-08-23 10:18:01 +0500

    Remove cellphone_number from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822113043) do
+ActiveRecord::Schema.define(version: 20160823051254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,7 +117,6 @@ ActiveRecord::Schema.define(version: 20160822113043) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "cellphone_number"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
