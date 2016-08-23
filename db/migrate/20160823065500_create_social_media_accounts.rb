c2dd0dc5bb290a5a314087e99f9b969258cb2c2d
amingilani <aminshahgilani@gmail.com>
2016-08-23 11:59:11 +0500

    Typo in migration

--- db/migrate/20160823065500_create_social_media_accounts.rb
+++ db/migrate/20160823065500_create_social_media_accounts.rb
@@ -4,7 +4,7 @@ class CreateSocialMediaAccounts < ActiveRecord::Migration[5.0]
       t.references :user, foreign_key: true
       t.integer :platform, null: false
       t.string :username, null: false
-      t.string :url null: false
+      t.string :url, null: false
       t.string :proof
       t.boolean :confirmed, null: false, default: false
 
