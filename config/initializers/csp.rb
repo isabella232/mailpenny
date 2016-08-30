f0e90d023905f75026f8f3aa1e6cd3932cf9cff1
amingilani <aminshahgilani@gmail.com>
2016-08-30 21:43:38 +0500

    Update CSP, report_only is false in production, and update report_uri

--- config/initializers/csp.rb
+++ config/initializers/csp.rb
@@ -1,15 +1,15 @@
 SecureHeaders::Configuration.default do |config|
   config.csp = {
-    report_only: Rails.env.production?, # default: false
+    report_only: !Rails.env.production?, # default: false
     preserve_schemes: true, # default: false.
 
 
-    default_src: %w(‘none’), # nothing allowed
+    default_src: %w('none'), # nothing allowed
     font_src: %w('self' fonts.gstatic.com),
     script_src: %w('self'),
     connect_src: %w('self'),
     img_src: %w('self'),
     style_src: %w('unsafe-inline' 'self' fonts.googleapis.com,),
-    report_uri: ["https://payload.report-uri.io/r/default/csp/reportOnly"]
+    report_uri: ['https://payload.report-uri.io/r/default/csp/enforce']
   }
 end
