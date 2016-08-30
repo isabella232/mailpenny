b59b1abd4903dc1b5498969794c820fded15c127
amingilani <aminshahgilani@gmail.com>
2016-08-30 21:55:10 +0500

    Revert session store file to default generated

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_mailman_session',
-                                                      secure: Rails.env.production?
+Rails.application.config.session_store :cookie_store, key: '_mailman_session'
