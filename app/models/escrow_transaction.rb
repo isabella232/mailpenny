e5549bce899ff7c76680135f96c351f0eff664d4
amingilani <aminshahgilani@gmail.com>
2016-08-31 06:15:24 +0500

    Annotate

--- app/models/escrow_transaction.rb
+++ app/models/escrow_transaction.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: escrow_transactions
+#
+#  id                     :integer          not null, primary key
+#  from_id                :integer
+#  to_id                  :integer
+#  is_open                :boolean
+#  state                  :integer
+#  opening_transaction_id :integer
+#  closing_transaction_id :integer
+#  amount                 :decimal(, )
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+
 class EscrowTransaction < ApplicationRecord
   belongs_to :from
   belongs_to :to
--- spec/models/escrow_transaction_spec.rb
+++ spec/models/escrow_transaction_spec.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: escrow_transactions
+#
+#  id                     :integer          not null, primary key
+#  from_id                :integer
+#  to_id                  :integer
+#  is_open                :boolean
+#  state                  :integer
+#  opening_transaction_id :integer
+#  closing_transaction_id :integer
+#  amount                 :decimal(, )
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe EscrowTransaction, type: :model do
