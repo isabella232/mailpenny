3e0eda7b91ab9deb6f7cd1533dbb6c4b27b086c1
amingilani <aminshahgilani@gmail.com>
2016-08-31 06:12:42 +0500

    Update migration

--- db/migrate/20160831010545_create_escrow_transactions.rb
+++ db/migrate/20160831010545_create_escrow_transactions.rb
@@ -1,16 +1,19 @@
 class CreateEscrowTransactions < ActiveRecord::Migration[5.0]
   def change
     create_table :escrow_transactions do |t|
-      t.references :from, foreign_key: true
-      t.references :to, foreign_key: true
+      t.references :from, index: true
+      t.references :to, index: true
       t.boolean :is_open
       t.integer :state
-      t.references :conversation, foreign_key: true
-      t.references :opening_transaction, foreign_key: true
-      t.references :closing_transaction, foreign_key: true
+      t.references :opening_transaction, index: true
+      t.references :closing_transaction, index: true
       t.decimal :amount
 
       t.timestamps
     end
+    add_foreign_key :escrow_transactions, :accounts, column: :from_id
+    add_foreign_key :escrow_transactions, :accounts, column: :to_id
+    add_foreign_key :escrow_transactions, :transactions, column: :opening_transaction_id
+    add_foreign_key :escrow_transactions, :transactions, column: :closing_transaction_id
   end
 end
