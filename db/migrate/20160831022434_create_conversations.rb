4229239eed0ef4a22d98729494c0744c502f2d8e
amingilani <aminshahgilani@gmail.com>
2016-08-31 09:20:16 +0500

    Update migration details

--- db/migrate/20160831022434_create_conversations.rb
+++ db/migrate/20160831022434_create_conversations.rb
@@ -2,12 +2,14 @@ class CreateConversations < ActiveRecord::Migration[5.0]
   def change
     create_table :conversations do |t|
       t.string :subject
-      t.references :initiator, foreign_key: true
-      t.references :recipient, foreign_key: true
+      t.references :initiator, index: true
+      t.references :recipient, index: true
       t.integer :status
-      t.references :escrow_transaction, foreign_key: true
+      t.references :escrow_transaction, index: true, foreign_key: true
 
       t.timestamps
     end
+    add_foreign_key :conversations, :users, column: :initiator_id
+    add_foreign_key :conversations, :users, column: :recipient_id
   end
 end
