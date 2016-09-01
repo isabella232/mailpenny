e52c2d5adc387b0d308a5dcdb7c0d57209d0926b
amingilani <aminshahgilani@gmail.com>
2016-09-01 05:10:17 +0500

    Run the migraiton

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831142032) do
+ActiveRecord::Schema.define(version: 20160901000917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160831142032) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "medium"
+    t.decimal  "fee_amount"
     t.index ["initiator_id"], name: "index_conversations_on_initiator_id", using: :btree
     t.index ["recipient_id"], name: "index_conversations_on_recipient_id", using: :btree
   end
