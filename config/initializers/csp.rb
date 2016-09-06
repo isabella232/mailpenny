c9a486e5ed97572199941d36abae0e71f1f723a3
amingilani <aminshahgilani@gmail.com>
2016-09-07 00:58:46 +0500

    Allow images from google analytics

--- config/initializers/csp.rb
+++ config/initializers/csp.rb
@@ -8,7 +8,7 @@ SecureHeaders::Configuration.default do |config|
     font_src: %w('self' fonts.gstatic.com),
     script_src: %w('self' www.google-analytics.com),
     connect_src: %w('self'),
-    img_src: %w('self'),
+    img_src: %w('self' www.google-analytics.com),
     style_src: %w('unsafe-inline' 'self' fonts.googleapis.com,),
     report_uri: ['https://payload.report-uri.io/r/default/csp/enforce']
   }
