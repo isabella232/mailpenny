9c8ff77e16e854a2de4db28fa17a2f2cca566cd4
amingilani <aminshahgilani@gmail.com>
2016-09-11 07:14:05 +0500

    Make account settings tests work

--- spec/feature/dashboard_account_settings_spec.rb
+++ spec/feature/dashboard_account_settings_spec.rb
@@ -2,16 +2,26 @@ require 'rails_helper'
 
 feature 'Dashboard' do
   background do
-    @user = create(:user) # create a new user
-    @user_changed = build(:user) # build an alternative user
+    @user = create :user  # create a new user
+    @user_changed = build :user  # build an alternative user
 
     # login as @user
-    visit '/login'
+    visit '/users/sign_in'
     within('#new_user') do
       fill_in 'Email', with: @user.email
       fill_in 'Password', with: @user.password
     end
     click_button 'Log in'
+  end
+
+  scenario 'fail to change username on incorrect password' do
+    visit '/dashboard/account'
+    within '#edit_user' do
+      fill_in 'Username', with: @user_changed.username
+      fill_in 'Current password', with: "incorrect_#{@user.password}"
+    end
+    click_button 'Update'
+    expect(page).to have_content 'password is invalid'
   end
 
   context 'in account settings' do
@@ -23,16 +33,6 @@ feature 'Dashboard' do
       end
       click_button 'Update'
       expect(page).to have_content 'Your account has been updated successfully.'
-    end
-
-    scenario 'fail to change username on incorrect password' do
-      visit '/dashboard/account'
-      within '#edit_user' do
-        fill_in 'Username', with: @user_changed.username
-        fill_in 'Current password', with: "incorrect_#{@user.password}"
-      end
-      click_button 'Update'
-      expect(page).to have_content 'password is invalid'
     end
   end
 end
