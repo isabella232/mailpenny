a5a02308a26ebc44da15de920a1273a27335271d
amingilani <aminshahgilani@gmail.com>
2016-09-13 21:21:28 +0500

    Move specs to appropriate controllers and add spec for about page

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -7,32 +7,4 @@ RSpec.describe StaticPagesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe 'GET #about' do
-    it 'returns http success' do
-      get :about
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET #terms' do
-    it 'returns http success' do
-      get :terms
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET #privacy' do
-    it 'returns http success' do
-      get :privacy
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET #help' do
-    it 'returns http success' do
-      get :help
-      expect(response).to have_http_status(:success)
-    end
-  end
 end
--- spec/controllers/text_pages_controller_spec.rb
+++ spec/controllers/text_pages_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe TextPagesController, type: :controller do
+  describe 'GET #about' do
+    it 'returns http success' do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
 
+  describe 'GET #tos' do
+    it 'returns http success' do
+      get :tos
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET #privacy' do
+    it 'returns http success' do
+      get :privacy
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
