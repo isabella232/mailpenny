501681a086dda0f37a6524f1f161530e938d46a6
amingilani <aminshahgilani@gmail.com>
2016-09-15 07:02:36 +0500

    Add method for profile links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,13 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(_resource)
     root_path
   end
+
+  private
+
+  # Returns the profile for the specified user
+  # @param [user] the user whose profile path is required
+  # @return [String] absolute path to the user's profile page
+  def user_profile_path(user)
+    "/#{user.username}"
+  end
 end
