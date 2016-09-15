df7d4a38ab161c662290c7b006b07244dd1480b5
amingilani <aminshahgilani@gmail.com>
2016-09-15 09:40:31 +0500

    Remove storage location from model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -41,8 +41,7 @@ class Profile < ApplicationRecord
   belongs_to :user
 
   has_attached_file :avatar, styles: { medium: '300x300>',
-                                       thumb: '100x100>' },
-                             default_url: '/assets/avatar.svg'
+                                       thumb: '100x100>' }
 
   validates_attachment_content_type :avatar, content_type: %r{\Aimage\/.*\z}
 
