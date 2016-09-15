4ba3e35f44c46eb30d5fe75932c098ee62c15719
amingilani <aminshahgilani@gmail.com>
2016-09-15 06:48:35 +0500

    Create update path for profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,27 @@
 # controller for the dashboard
 class ProfilesController < ApplicationController
   before_action :authenticate_user!, only: [:create, :update]
+  before_action :set_profile, only: [:update]
 
+  # PATCH/PUT /profiles/1
+  # PATCH/PUT /profiles/1.json
   def update
+    respond_to do |format|
+      if @profile.update(profile_params)
+        format.html { redirect_to @profile, notice: 'Profile was successfully updated.' }
+        format.json { render :show, status: :ok, location: @profile }
+      else
+        format.html { render :edit }
+        format.json { render json: @profile.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
+
+  def set_profile
+    @profile = Profile.find(params[:id])
+  end
 
   def profile_params
     params.require(:user).permit(:avatar)
