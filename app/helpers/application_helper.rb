fa47fb5429fc8faf597afe4ecc692e3c459bd1eb
amingilani <aminshahgilani@gmail.com>
2016-09-16 07:35:28 +0500

    Change flash messages to bootstrap class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,17 @@
 # The parent helper for all helpers
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    case flash_type
+    when 'success'
+      'success' # Green
+    when 'error'
+      'danger' # Red
+    when 'alert'
+      'warning' # Yellow
+    when 'notice'
+      'info' # Blue
+    else
+      flash_type
+    end
+  end
 end
