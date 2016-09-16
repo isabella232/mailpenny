ea6fc099a886f95e4fa462be53936a2d0da5f094
amingilani <aminshahgilani@gmail.com>
2016-09-16 17:31:59 +0500

    Comment out redundant parts of message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,65 +1,62 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
 
-  # GET /messages
-  # GET /messages.json
-  def index
-    @messages = Message.all
-  end
+  # # GET /messages
+  # # GET /messages.json
+  # def index
+  #   @messages = Message.all
+  # end
 
-  # GET /messages/1
-  # GET /messages/1.json
-  def show
-  end
+  # # GET /messages/1
+  # # GET /messages/1.json
+  # def show
+  # end
 
-  # GET /messages/new
-  def new
-    @message = Message.new
-  end
+  # # GET /messages/new
+  # def new
+  #   @message = Message.new
+  # end
 
-  # GET /messages/1/edit
-  def edit
-  end
+  # # GET /messages/1/edit
+  # def edit
+  # end
 
   # POST /messages
   # POST /messages.json
   def create
     @message = Message.new(message_params)
-
-    respond_to do |format|
-      if @message.save
-        format.html { redirect_to @message, notice: 'Message was successfully created.' }
-        format.json { render :show, status: :created, location: @message }
-      else
-        format.html { render :new }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
+    @message.conversation = Conversation.find(conversation_id_param[:id])
+    @message.sender = current_user
+    if @message.save
+      redirect_to :back, notice: 'Message was successfully created.'
+    else
+      redirect_to :back, notice: 'Message was not sent!'
     end
   end
 
-  # PATCH/PUT /messages/1
-  # PATCH/PUT /messages/1.json
-  def update
-    respond_to do |format|
-      if @message.update(message_params)
-        format.html { redirect_to @message, notice: 'Message was successfully updated.' }
-        format.json { render :show, status: :ok, location: @message }
-      else
-        format.html { render :edit }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # # PATCH/PUT /messages/1
+  # # PATCH/PUT /messages/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @message.update(message_params)
+  #       format.html { redirect_to @message, notice: 'Message was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @message }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @message.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # DELETE /messages/1
-  # DELETE /messages/1.json
-  def destroy
-    @message.destroy
-    respond_to do |format|
-      format.html { redirect_to messages_url, notice: 'Message was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # # DELETE /messages/1
+  # # DELETE /messages/1.json
+  # def destroy
+  #   @message.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to messages_url, notice: 'Message was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
 
@@ -70,6 +67,10 @@ class MessagesController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def message_params
-    params.fetch(:message, {})
+    params.require(:message).permit(:body)
+  end
+
+  def conversation_id_param
+    params.require(:conversation).permit(:id)
   end
 end
