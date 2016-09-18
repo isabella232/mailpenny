400e8f4615d5189fe6b3abfb6137d29b854afc81
amingilani <aminshahgilani@gmail.com>
2016-09-18 13:06:21 +0500

    Add email configuration for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,17 +57,6 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  # Use a real queuing backend for Active Job (and separate queues per
-  # environment)
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "mailman_#{Rails.env}"
-  config.action_mailer.perform_caching = false
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to
-  # raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
@@ -94,7 +83,23 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # action mailer settings
+  # Use a real queuing backend for Active Job (and separate queues per environment)
+  # config.active_job.queue_adapter     = :resque
+  # config.active_job.queue_name_prefix = "mailman_#{Rails.env}"
+  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'mailpenny.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    tls:                  true,
+    address:              'smtp.mailgun.org',
+    port:                 465,
+    domain:   'mailpenny.com',
+    user_name: ENV['SMTP_LOGIN'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: :plain
+  }
 
   config.paperclip_defaults = {
     storage: :s3,
