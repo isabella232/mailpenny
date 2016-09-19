f441d28efd37dd589b60bbad1e0076579433b807
amingilani <aminshahgilani@gmail.com>
2016-09-19 13:36:55 +0500

    Don't clean db before and after ordered tests

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -11,11 +11,11 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :truncation
   end
 
-  config.before(:each) do
+  config.before(:each, unless: { order: :defined }) do
     DatabaseCleaner.start
   end
 
-  config.after(:each) do
+  config.after(:each, unless: { order: :defined }) do
     DatabaseCleaner.clean
   end
 end
