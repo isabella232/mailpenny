d4f1e55b6964b7f66b450adf3b8dfb753bd6c324
amingilani <aminshahgilani@gmail.com>
2016-09-20 16:59:03 +0500

    Fluff commit

--- app/models/message.rb
+++ app/models/message.rb
@@ -36,6 +36,7 @@
 #
 # <!-- END GENERATED ANNOTATION -->
 
+# The messages from the body
 class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :sender, foreign_key: :sender_id, class_name: 'User'
