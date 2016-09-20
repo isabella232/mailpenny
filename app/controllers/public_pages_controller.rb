89ed57367f20ca6259df23a1c1160ded7294d060
amingilani <aminshahgilani@gmail.com>
2016-09-20 15:31:08 +0500

    Simplify methods and remove redundancies

--- app/controllers/public_pages_controller.rb
+++ app/controllers/public_pages_controller.rb
@@ -9,13 +9,10 @@ class PublicPagesController < ApplicationController
   def profile
     profile_username = params[:username]
     @profile_user = User.find_by(username: profile_username)
-    not_found if @profile_user.nil?
     @profile = @profile_user.profile
     not_found if @profile.nil?
 
     @conversation = Conversation.new if user_signed_in?
     render 'own_profile' if user_signed_in? && current_user == @profile_user
   end
-
-  private
 end
