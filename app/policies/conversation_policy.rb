527dc4e67f6fc78bd34074b8ec9d5249d983951f
amingilani <aminshahgilani@gmail.com>
2016-09-20 15:38:39 +0500

    Add a pundit policy to authorize conversation creation

--- app/policies/conversation_policy.rb
+++ app/policies/conversation_policy.rb
@@ -2,6 +2,10 @@ class ConversationPolicy < ApplicationPolicy
   def show?
     @record.users.include? @user
   end
+
+  def create?
+    @record.initiator == @user
+  end
   class Scope < Scope
     def resolve
       scope
