3b476cbd0c75d84dd8976542389a619c707905d5
amingilani <aminshahgilani@gmail.com>
2016-09-22 23:08:06 +0500

    Remove intercom config

--- config/initializers/csp.rb
+++ config/initializers/csp.rb
@@ -5,9 +5,9 @@ SecureHeaders::Configuration.default do |config|
 
 
     default_src: %w('none'), # nothing allowed
-    font_src: %w('self' fonts.gstatic.com *.intercom.io *.intercomcdn.com),
-    script_src: %w('self' www.google-analytics.com *.intercom.io *.intercomcdn.com),
-    connect_src: %w('self' wss://*.intercom.io *.intercom.io *.intercomcdn.com),
+    font_src: %w('self' fonts.gstatic.com),
+    script_src: %w('self' www.google-analytics.com),
+    connect_src: %w('self'),
     img_src: %w('self' www.google-analytics.com),
     style_src: %w('unsafe-inline' 'self' fonts.googleapis.com,),
     report_uri: ['https://payload.report-uri.io/r/default/csp/enforce']
