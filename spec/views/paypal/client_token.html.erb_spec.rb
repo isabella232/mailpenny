02484e4f660963ed2eedf4f077229755d4780c65
amingilani <aminshahgilani@gmail.com>
2016-09-30 05:16:48 +0500

    Rubocop autocorrect

--- spec/views/paypal/client_token.html.erb_spec.rb
+++ spec/views/paypal/client_token.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "paypal/client_token.html.erb", type: :view do
+RSpec.describe 'paypal/client_token.html.erb', type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
 end
