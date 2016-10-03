fe2ab866b4cdcfba8be2cfe0eae01d719ae3696d
amingilani <aminshahgilani@gmail.com>
2016-10-03 14:57:53 +0500

    Add deposit function and broken withdrawals

--- app/controllers/payments_handler_controller.rb
+++ app/controllers/payments_handler_controller.rb
@@ -1,7 +1,27 @@
 class PaymentsHandlerController < ApplicationController
+  before_action :authenticate_user!
+
   def deposit
+    params = deposit_params
+    raise 'Test is not available in production' if params[:method] == 'test' && Rails.env.production?
+    amount = params[:amount].to_d
+    current_user.account.deposit amount
   end
 
   def withdrawal
+    params = withdrawal_params
+    raise 'Test is not available in production' if params[:method] == 'test' && Rails.env.production?
+    amount = params[:amount].to_d
+    current_user.account.withdraw amount
   end
+
+  private
+
+    def deposit_params
+      params.permit(:method, :amount)
+    end
+
+    def withdrawal
+      params.permit(:method, :amount)
+    end
 end
