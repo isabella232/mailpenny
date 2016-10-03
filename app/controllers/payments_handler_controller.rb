36655f1bde7ce4ab8fadd4107fc1afe601d80886
amingilani <aminshahgilani@gmail.com>
2016-10-03 16:29:33 +0500

    Add deposit hanlders

--- app/controllers/payments_handler_controller.rb
+++ app/controllers/payments_handler_controller.rb
@@ -3,16 +3,26 @@ class PaymentsHandlerController < ApplicationController
 
   def deposit
     params = deposit_params
-    raise 'Test is not available in production' if params[:method] == 'test' && Rails.env.production?
-    amount = params[:amount].to_d
-    current_user.account.deposit amount
+    if params[:method] == 'test' && Rails.env.production?
+      amount = params[:amount].to_d
+      current_user.account.withdraw amount
+    elsif params[:method] == 'test'
+      raise 'Test is not available in production'
+    else
+      # TODO handle legit deposits
+    end
   end
 
   def withdrawal
     params = withdrawal_params
-    raise 'Test is not available in production' if params[:method] == 'test' && Rails.env.production?
-    amount = params[:amount].to_d
-    current_user.account.withdraw amount
+    if params[:method] == 'test' && Rails.env.production?
+      amount = params[:amount].to_d
+      current_user.account.withdraw amount
+    elsif params[:method] == 'test'
+      raise 'Test is not available in production'
+    else
+      # TODO handle legit deposits
+    end
   end
 
   private
